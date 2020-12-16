d39ac7b19f0eca981e6b15187caafd8dba2362b2
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-12-16 17:07:11 +0100

    Add project files.

---
+++ APIdata/APIdata/Model/AllCurentObjects.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace APIdata.Model
+{
+    public class AllCurentObjects
+    {
+        public ObservableCollection<Telesa> AllObjects { get; set; }
+        public AllCurentObjects()
+        {
+        }
+
+        public async Task GetFromAPI()
+        {
+            HttpClient http = new HttpClient();
+            try
+            {
+                DateTime dt = DateTime.Now;
+                DateTime dt2 = dt.AddDays(-1);
+                string url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + dt2.ToString("yyyy-MM-dd") + "&end_date=" + dt.ToString("yyyy-MM-dd") + "&api_key=d5m1LesAjClwYjHv5pDC572UnkZOhKls0jOomejg";
+                HttpResponseMessage response = await http.GetAsync(url, HttpCompletionOption.ResponseContentRead);
+                string res = await response.Content.ReadAsStringAsync();
+                var content = JsonConvert.DeserializeObject<dynamic>(res);
+
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ APIdata/APIdata/Model/Telesa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace APIdata.Model
+{
+    public class Telesa
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("estimated_diameter_min")]
+        public double Width { get; set; }
+
+        [JsonProperty("estimated_diameter_max")]
+        public double Height { get; set; }
+        public double KMPerHour { get; set; }
+        public double MissDistance { get; set; }
+    }
+}
